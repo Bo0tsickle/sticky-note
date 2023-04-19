971b4c03574895590b53f23aae38b6f568ffab6b
Bo0tsickle <zelda.geek616@gmail.com>
2023-04-18 21:55:17 -0700

    file renaming

--- tasks.cpp
+++ tasks.cpp
@@ -2,17 +2,18 @@
 #include <string>
 using std::string;
 
-class tasks {
+class Tasks {
     private:
         string _title = "NULL";
         string _description = "NULL";
         int _priority = 0;
     
     public:
-        tasks(string title, string description, int priority)
+        Tasks(string title, string description, int priority)
         : _title(title), _description(description), _priority(priority)
         {}
-        ~tasks();
+        void WriteTitleToFile        
+        ~Tasks();
 };
 
-tasks::~tasks() {}
+tasks::~Tasks() {}
