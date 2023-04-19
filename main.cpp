d1482c8fd693d26171dff344233965a98bdf57c9
Bo0tsickle <zelda.geek616@gmail.com>
2023-04-18 21:42:46 -0700

    created class

---
+++ main.cpp
@@ -0,0 +1,7 @@
+#include <iostream>
+using std::cout;
+
+int main() {
+    cout << "Welcome to the sticky notes app! Press: \n(1) Create Note\n(2) Delete Note\n(3) List Notes";
+    return 0;
+}
---
+++ tasks.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+#include <string>
+
+class tasks {
+    private:
+    std::string title = "NULL";
+    std::string description = "NULL";
+    int priority = 0;
+    
+    public:
+        tasks(/* args */);
+        ~tasks();
+};
+
+tasks::tasks(/* args */) {
+
+}
