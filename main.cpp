9f797ba7609bb52ed7634ef2ee7ccd4a87b1067b
Bo0tsickle <zelda.geek616@gmail.com>
2023-04-18 22:22:10 -0700

    im giddy to see if writing to file in cpp is ez

--- main.cpp
+++ main.cpp
@@ -1,7 +1,10 @@
 #include <iostream>
+#include "Tasks.h"
 using std::cout;
 
 int main() {
     cout << "Welcome to the sticky notes app! Press: \n(1) Create Note\n(2) Delete Note\n(3) List Notes";
+    Tasks task = Tasks("deez", "nuts", 69);
+    task.WriteTaskToFile();
     return 0;
 }
